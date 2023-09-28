892748fa48e284774f6085ad76617efe8e207789
kanbrtkuy <barryindalhousie@gmail.com>
2023-09-28 03:04:24 -0500

    created reactapp and connected it to django

---
+++ present_django/reactapp/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="center-colum">
+        <div className='item-row'>
+          <span>One</span>
+        </div>
+        <div className='item-row'>
+          <span>Two</span>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
